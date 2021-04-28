91b77fbb2410df4ebc85ed0f94a7df92669d2600
Raphael Haecker <raphael.haecker@gmail.com>
2021-04-29 00:40:22 +0200

    Added a connection to the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot()
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
