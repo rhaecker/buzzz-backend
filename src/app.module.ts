6e013b3431faf4422005984d27b178c8fdaaa70b
Raphael Haecker <raphael.haecker@gmail.com>
2021-05-04 01:27:47 +0200

    Database config from .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,23 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongoDBConfigService } from './config-services/mongodbconfig.service';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot()
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: MongoDBConfigService,
+      inject: [MongoDBConfigService]
+    })
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, MongoDBConfigService],
 })
+
 export class AppModule {}
+
